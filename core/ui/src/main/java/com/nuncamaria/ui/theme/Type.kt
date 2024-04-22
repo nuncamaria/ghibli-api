0db632f5aa1df0e621ce16211c5854cb753eb6b8
nuncamaria <nunezcanovas@gmail.com>
2024-04-22 12:53:50 +0200

    Added Spacing and Type custom values

--- core/ui/src/main/java/com/nuncamaria/ui/theme/Type.kt
+++ core/ui/src/main/java/com/nuncamaria/ui/theme/Type.kt
@@ -2,33 +2,60 @@ package com.nuncamaria.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.nuncamaria.ui.R
 
-// Set of Material typography styles to start with
+private val Kyn = FontFamily(
+    Font(resId = R.font.kyn_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.kyn_semibold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.kyn_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.kyn_regular, weight = FontWeight.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    headlineLarge = TextStyle(
+        fontFamily = Kyn,
+        fontWeight = FontWeight.Medium,
+        fontSize = 34.sp,
+        lineHeight = 38.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Kyn,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Kyn,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Kyn,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    bodyLarge = TextStyle(
+        fontFamily = Kyn,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Kyn,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
